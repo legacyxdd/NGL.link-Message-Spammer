1b057ffe321b093dd76f0350c1f1249f0888bf25
Łegacy <72857637+yigitlegacy@users.noreply.github.com>
2022-12-01 16:35:15 +0300

    Update Spammer.py

--- Spammer.py
+++ Spammer.py
@@ -16,9 +16,9 @@ os.system("cls")
 r = requests.Session()
 print(f"{Fore.LIGHTCYAN_EX}NGL App Spammer Tool, Use it on your own risk...")
 # Target is the visible username for the target, not the full link.
-target = input(f"{Fore.GREEN}Enter {Fore.RED}Target: {Fore.WHITE}")
-question = input(f"{Fore.GREEN}Enter {Fore.RED}Your Message: {Fore.WHITE}")
-start = input(f"{Fore.WHITE}Start the {Fore.GREEN}Attack? Y/N: {Fore.WHITE}").upper()
+target = input(f"{Fore.RED}Enter {Fore.RED}Target: {Fore.WHITE}")
+question = input(f"{Fore.RED}Enter {Fore.RED}Your Message: {Fore.WHITE}")
+start = input(f"{Fore.WHITE}Start the {Fore.WHITE}Attack? Y/N: {Fore.WHITE}").upper()
 def spam():
     questions_count = 0
     url = f"https://ngl.link/{target}"
@@ -32,7 +32,7 @@ def spam():
     while True:
         spam = r.post(url, headers=headers, data=payload)
         questions_count += 1
-        print(f"{Fore.GREEN}{questions_count} {Fore.WHITE}Questions Sent to: {Fore.RED}{target}")
+        print(f"{Fore.WHITE}{questions_count} {Fore.WHITE}Questions Sent to: {Fore.RED}{target}")
         
 if start == "Y":
     spam()
